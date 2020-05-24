0919686fa224aaa0ba2c64dd88faa36a17432c44
swaggermeister <swaggermeister@noreply.github.com>
2020-05-24 18:29:30 -0400

    add markdown preview for job description creation

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -13,6 +13,11 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
+import { setupClickHandlers } from "../preview_description";
+document.addEventListener("DOMContentLoaded", () => {
+  setupClickHandlers();
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
