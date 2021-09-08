d63dfef157b8f856421dd254aadbf69eab937741
swaggermeister <3065224+swaggermeister@users.noreply.github.com>
2021-09-08 14:15:18 -0400

    Rubocop fix

--- db/migrate/20200525181319_add_contact_info_to_job_listings.rb
+++ db/migrate/20200525181319_add_contact_info_to_job_listings.rb
@@ -2,9 +2,9 @@
 
 class AddContactInfoToJobListings < ActiveRecord::Migration[6.0]
   def change
-    # rubocop:disable Rails/BulkChangeTable
-    add_column :job_listings, :contact_email, :string
-    add_column :job_listings, :contact_url, :string
-    # rubocop:enable Rails/BulkChangeTable
+    change_table :job_listings, bulk: true do |t|
+      t.string :contact_email
+      t.string :contact_url
+    end
   end
 end
