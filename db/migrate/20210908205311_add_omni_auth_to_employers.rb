50f3a6e148107931bf0172a0dc1859b6c45d02c8
swaggermeister <3065224+swaggermeister@users.noreply.github.com>
2021-09-10 13:51:14 -0400

    remove safety option from oauth migration file, not needed for prod

--- db/migrate/20210908205311_add_omni_auth_to_employers.rb
+++ db/migrate/20210908205311_add_omni_auth_to_employers.rb
@@ -2,11 +2,9 @@
 
 class AddOmniAuthToEmployers < ActiveRecord::Migration[6.0]
   def change
-    safety_assured do
-      change_table :employers, bulk: true do |t|
-        t.string :provider
-        t.string :uid
-      end
+    change_table :employers, bulk: true do |t|
+      t.string :provider
+      t.string :uid
     end
   end
 end
